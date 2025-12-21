daab10389256df301d2b5945483e216a26ed30c1
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-22 01:44:27 +0530

    Add Vercel Analytics integration with @vercel/analytics package and Analytics component in App router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ExperiencePage from './pages/ExperiencePage';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/experience" element={<ExperiencePage />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
